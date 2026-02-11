abb630237cbbec18e8ef792860d67555aacbb351
Drwei <weishixvn@outlook.com>
2026-02-11 20:22:24 +0800

    Update fabric-loom version and clean up build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,20 @@
 plugins {
     java
-    id("fabric-loom") version "1.3-SNAPSHOT"
+    id("fabric-loom") version "1.3"
     `maven-publish`
 }
 
-version = "1.0.0"
-group = "com.airextra"
-
 base {
     archivesName = "AirExtra"
 }
 
+loom {
+    accessWidenerPath = file("src/main/resources/airextra.accesswidener")
+}
+
 repositories {
+    mavenLocal()
+    maven { url = uri("https://maven.fabricmc.net/") }
     mavenCentral()
 }
 
@@ -21,6 +24,9 @@ dependencies {
     modImplementation("net.fabricmc:fabric-loader:0.15.11")
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.100.1+1.21.11")
 }
+
+version = "1.0.0"
+group = "com.airextra"
 
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
@@ -35,41 +41,6 @@ java {
 
 jar {
     from("LICENSE") {
-        rename { "${it}_${base.archivesName.get()}"}
-    }
-}
-
-version = "1.0.0"
-group = "com.airextra"
-
-base {
-    archivesName = "AirExtra"
-}
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    minecraft("com.mojang:minecraft:1.21.11")
-    mappings("net.fabricmc:yarn:1.21.11+build.4:v2")
-    modImplementation("net.fabricmc:fabric-loader:0.15.11")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.100.1+1.21.11")
-}
-
-tasks.withType<JavaCompile> {
-    options.encoding = "UTF-8"
-    options.release = 21
-}
-
-java {
-    withSourcesJar()
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
-}
-
-jar {
-    from("LICENSE") {
-        rename { "${it}_${base.archivesName.get()}"}
+        rename { "${it}_${base.archivesName.get()}" }
     }
 }
