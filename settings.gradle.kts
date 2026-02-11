a3d2b10231bcfd92da303836c45b28c082ebdb7f
Drwei <weishixvn@outlook.com>
2026-02-11 20:21:42 +0800

    Configure plugin management in settings.gradle.kts
    Added plugin management repositories for Gradle.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,10 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        maven { url = uri("https://maven.fabricmc.net/") }
+        maven { url = uri("https://server.bbkr.space/artifactory/libs-release/") }
+        gradlePluginPortal()
+    }
+}
+
 rootProject.name = "AirExtra"
