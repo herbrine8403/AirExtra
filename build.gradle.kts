5b6480485e72d0f988fe3abe0c13cbc7ee43562a
herbrine8403 <weishixvn@outlook.com>
2026-02-11 20:10:07 +0800

    fix: 恢复到原始配置（fabric-loom:1.3-SNAPSHOT）

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,17 @@
-pluginManagement {
-    repositories {
-        mavenLocal()
-        maven { url = uri("https://maven.fabricmc.net/") }
-        maven { url = uri("https://server.bbkr.space/artifactory/libs-release/") }
-        gradlePluginPortal()
-    }
-}
-
 plugins {
     java
-    id("fabric-loom") version "1.3"
+    id("fabric-loom") version "1.3-SNAPSHOT"
     `maven-publish`
 }
+
+version = "1.0.0"
+group = "com.airextra"
 
 base {
     archivesName = "AirExtra"
 }
 
-loom {
-    accessWidenerPath = file("src/main/resources/airextra.accesswidener")
-}
-
 repositories {
-    mavenLocal()
-    maven { url = uri("https://maven.fabricmc.net/") }
     mavenCentral()
 }
 
@@ -32,6 +20,23 @@ dependencies {
     mappings("net.fabricmc:yarn:1.21.11+build.4:v2")
     modImplementation("net.fabricmc:fabric-loader:0.15.11")
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.100.1+1.21.11")
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.release = 21
+}
+
+java {
+    withSourcesJar()
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+jar {
+    from("LICENSE") {
+        rename { "${it}_${base.archivesName.get()}"}
+    }
 }
 
 version = "1.0.0"
