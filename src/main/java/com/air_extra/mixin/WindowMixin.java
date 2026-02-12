e81a8d1e67147e318ba0179a6353f720e0c0d358
herbrine8403 <weishixvn@outlook.com>
2026-02-12 10:46:38 +0800

    fix: Remove client shadow field from WindowMixin

--- src/main/java/com/air_extra/mixin/WindowMixin.java
+++ src/main/java/com/air_extra/mixin/WindowMixin.java
@@ -5,8 +5,6 @@ import com.air_extra.config.AirExtraConfig;
 import com.air_extra.feature.ToastHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.Window;
-import org.lwjgl.glfw.GLFW;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,8 +13,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Window.class)
 public class WindowMixin {
-    
-    @Shadow @Final private MinecraftClient client;
     
     @Shadow private int width;
     @Shadow private int height;
@@ -32,9 +28,11 @@ public class WindowMixin {
             
             if (AirExtraClient.isModEnabled()) {
                 AirExtraConfig config = AirExtraClient.getConfig();
+                MinecraftClient client = MinecraftClient.getInstance();
+                
                 if (config != null && config.isEnablePortraitCheck()) {
                     boolean isPortrait = height > width;
-                    if (isPortrait && client.player != null) {
+                    if (isPortrait && client != null && client.player != null) {
                         ToastHelper.showWarningToast(client, config.portraitWarningText);
                     }
                     
